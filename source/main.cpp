c36358094893a57377ba908ab7a5919f31ad319d
Hangyaku <285472965+hangyaku-motome@users.noreply.github.com>
2026-04-25 17:45:31 +0300

    Imported Dear ImGui + Implemented it for the first time.

---
+++ source/main.cpp
@@ -0,0 +1,115 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+static void glfw_error_callback(int error, const char *description) {
+  fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+int main() {
+  glfwSetErrorCallback(glfw_error_callback);
+
+  if (!glfwInit())
+    return 1;
+
+  const char *glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+  float main_scale =
+      ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor());
+
+  GLFWwindow *window =
+      glfwCreateWindow((int)(1280 * main_scale), (int)(800 * main_scale),
+                       "My_MemScanner", nullptr, nullptr);
+  if (window == nullptr)
+    return 1;
+
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+  ImGui::StyleColorsDark();
+
+  ImGuiStyle &style = ImGui::GetStyle();
+  style.ScaleAllSizes(main_scale);
+  style.FontScaleDpi = main_scale;
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+
+  ImGui_ImplOpenGL3_Init(glsl_version);
+
+  bool show_demo_window = false;
+  bool show_another_window = false;
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  bool LikesIceCream = false;
+
+  // Main loop.
+
+  while (!glfwWindowShouldClose(window)) {
+
+    glfwPollEvents();
+
+    if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0) {
+      ImGui_ImplGlfw_Sleep(10);
+      continue;
+    }
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    {
+      static float f = 0.0f;
+      static int counter = 0;
+
+      ImGui::Begin("Hello.");
+
+      ImGui::Text("Hopefully this works.");
+      ImGui::Checkbox("Do you like vanilla ice cream?", &LikesIceCream);
+      ImGui::SameLine();
+      if (LikesIceCream)
+        ImGui::Text("Wow me too!");
+
+      ImGui::NewLine();
+
+      if (ImGui::Button("Click to make numbers go up."))
+        ++counter;
+
+      ImGui::Text("number = %d", counter);
+
+      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                  1000.0f / io.Framerate, io.Framerate);
+
+      ImGui::End();
+    }
+
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
+                 clear_color.z * clear_color.w, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+  }
+
+  // Clean up.
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+}
