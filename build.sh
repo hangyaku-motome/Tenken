14ee6025bf905bc625c765f6146ef25ee22cebbe
Hangyaku <285472965+hangyaku-motome@users.noreply.github.com>
2026-06-24 19:02:11 +0300

    small edit to build.sh

--- build.sh
+++ build.sh
@@ -6,15 +6,18 @@ if [ "$1" = "win" ]; then
 elif [ "$1" = "debug" ]; then
     cmake -B build-linux -DCMAKE_BUILD_TYPE=Debug
     cmake --build build-linux
+    ln -sf build-linux/compile_commands.json compile_commands.json
 elif [ "$1" = "san" ]; then
     cmake -B build-linux -DCMAKE_BUILD_TYPE=Debug -DENABLE_SAN=ON
     cmake --build build-linux
+    ln -sf build-linux/compile_commands.json compile_commands.json
 elif [ "$1" = "tsan" ]; then
     cmake -B build-linux -DCMAKE_BUILD_TYPE=Debug -DENABLE_TSAN=ON
     cmake --build build-linux
+    ln -sf build-linux/compile_commands.json compile_commands.json
 else
     cmake -B build-linux -DCMAKE_BUILD_TYPE=Release
     cmake --build build-linux
+    ln -sf build-linux/compile_commands.json compile_commands.json
 fi
 
-ln -sf build-linux/compile_commands.json compile_commands.json
