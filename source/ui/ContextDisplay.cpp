e0f61f50f43eddd036ae4d63548e9b13a320f13b
Hangyaku <285472965+hangyaku-motome@users.noreply.github.com>
2026-08-12 20:18:17 +0300

    improving context display

--- source/ui/ContextDisplay.cpp
+++ source/ui/ContextDisplay.cpp
@@ -57,3 +57,70 @@ float ContextDisplay::drawRefreshInterval(const float RefreshDuration) {
 
   return returnval;
 }
+
+template <typename T>
+PendingAction ContextDisplay::cycleContext(const ReadBlock& context,
+                                           uint64_t selected_row,
+                                           uint32_t target_size,
+                                           float refresh_seconds) {
+  if (refresh_seconds >= 0)
+    is_refresh_ = true;
+  else
+    is_refresh_ = false;
+
+  PendingAction action{};
+
+  drawContextMenu(context, target_size);
+
+  alignButtons();
+
+  auto refresh_context = drawRefreshContextButton();
+
+  ImGui::SameLine();
+  float new_refresh_duration = drawRefreshInterval(refresh_seconds);
+  if (new_refresh_duration != -2) {
+    if constexpr (std::is_same_v<T, HitInfo>)
+      action = Action::Hit::RegularRefresh(new_refresh_duration);
+    else
+      action = Action::Favourite::RegularRefresh(new_refresh_duration);
+  }
+  ImGui::SameLine();
+  auto refresh_all = drawRefreshAllButton();
+
+  if (refresh_context) {
+    if constexpr (std::is_same_v<T, HitInfo>)
+      action = Action::Hit::Rescan(selected_row);
+    else
+      action = Action::Favourite::Rescan(selected_row);
+  }
+
+  if (refresh_all) {
+    if constexpr (std::is_same_v<T, HitInfo>)
+      action = Action::Hit::RescanAll{};
+    else
+      action = Action::Favourite::RescanAll{};
+  }
+
+  return action;
+};
+
+template PendingAction ContextDisplay::cycleContext<HitInfo>(const ReadBlock&, uint64_t, uint32_t, float);
+template PendingAction ContextDisplay::cycleContext<FavouriteInfo>(const ReadBlock&, uint64_t, uint32_t, float);
+
+void ContextDisplay::drawContextMenu(const ReadBlock& context, uint32_t target_size) {
+  for (int64_t i = 0; i < context.read_bytes.size(); ++i) {
+    ImGui::SameLine(0, 4);
+    if (i % 32 == 0)
+      ImGui::NewLine();
+    else if (i % 8 == 0) {
+      ImGui::Text(" ");
+      ImGui::SameLine(0, 4);
+    }
+    if (i + context.offset_from_adr >= 0 && i + context.offset_from_adr < target_size) {
+      ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 160, 100, 255));
+      ImGui::Text("%02X", context.read_bytes[i]);
+      ImGui::PopStyleColor();
+    } else
+      ImGui::Text("%02X", context.read_bytes[i]);
+  }
+}
--- source/ui/ContextDisplay.h
+++ source/ui/ContextDisplay.h
@@ -1,31 +1,13 @@
 #pragma once
 
 #include <cstdint>
-#include <variant>
 
 #include "types.h"
 
-namespace ContextIntent {
-struct Rescan {
-  uint64_t index;
-};
-
-struct RescanAll {};
-
-struct Write {
-  uint64_t index;
-};
-
-struct RegularRefresh {
-  float seconds;
-};
-}  // namespace ContextIntent
-
-using ContextResult = std::variant<std::monostate,
-                                   ContextIntent::Rescan,
-                                   ContextIntent::RescanAll,
-                                   ContextIntent::Write,
-                                   ContextIntent::RegularRefresh>;
+// okay wow this is actually kind of garbage
+// ehh well it's marginally better now but we could make it arguably better (or slightly worse?). (passing HitInfo or
+// FavouriteInfo instead of context and target_size).
+// TODO: maybe I'll fix it up more in the future
 
 class ContextDisplay {
   float button_w_ = 150.0F;
@@ -39,81 +21,10 @@ class ContextDisplay {
   bool drawRefreshContextButton() const;
   void alignButtons();
 
-  template <typename T> static void drawContextMenu(const T entry) {
-    if (entry.value.size() + BytesBefore + BytesAfter != entry.bytes_around.size()) {
-      return;
-    }
-
-    for (uint64_t i = 0; i < entry.bytes_around.size(); ++i) {
-      ImGui::SameLine(0, 4);
-      if (i % 32 == 0)
-        ImGui::NewLine();
-      else if (i % 8 == 0) {
-        ImGui::Text(" ");
-        ImGui::SameLine(0, 4);
-      }
-      if (i >= BytesBefore && i + BytesAfter < entry.bytes_around.size()) {
-        ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 160, 100, 255));
-        ImGui::Text("%02X", entry.bytes_around[i]);
-        ImGui::PopStyleColor();
-      } else
-        ImGui::Text("%02X", entry.bytes_around[i]);
-    }
-  }
+  void drawContextMenu(const ReadBlock& context, uint32_t target_size);
 
 public:
-  template <typename T> ContextResult cycleContext(const uint64_t selected_row, const T entry, float refresh_seconds) {
-    if (refresh_seconds >= 0)
-      is_refresh_ = true;
-    else
-      is_refresh_ = false;
-
-    ContextResult ReturnAction{};
-
-    drawContextMenu(entry);
-
-    alignButtons();
-
-    auto RefreshContext = drawRefreshContextButton();
-
-    ImGui::SameLine();
-    float NewRefreshDuration = drawRefreshInterval(refresh_seconds);
-    if (NewRefreshDuration != -2) {
-      ReturnAction = ContextIntent::RegularRefresh(NewRefreshDuration);
-    }
-    ImGui::SameLine();
-    auto RefreshAll = drawRefreshAllButton();
-
-    if (RefreshContext) return ContextIntent::Rescan{selected_row};
-    if (RefreshAll) return ContextIntent::RescanAll{};
-
-    return ReturnAction;
-  }
-
-  PendingAction ResolveContextIntent(ContextResult& cont, bool IsHitWindow) {
-    PendingAction result{};
-    std::visit(overloaded{
-                   [&](ContextIntent::Rescan& c) -> void {
-                     if (IsHitWindow)
-                       result = Action::Hit::Rescan{c.index};
-                     else
-                       result = Action::Favourite::Rescan{c.index};
-                   },
-                   [&](ContextIntent::RescanAll&) -> void {
-                     if (IsHitWindow)
-                       result = Action::Hit::RescanAll{};
-                     else
-                       result = Action::Favourite::RescanAll{};
-                   },
-                   [&](ContextIntent::RegularRefresh& c) -> void {
-                     if (IsHitWindow)
-                       result = Action::Hit::RegularRefresh{c.seconds};
-                     else
-                       result = Action::Favourite::RegularRefresh{c.seconds};
-                   },
-                   [&](auto&) {},
-               },
-               cont);
-    return result;
-  }
+  template <typename T>
+  PendingAction
+  cycleContext(const ReadBlock& context, uint64_t selected_row, uint32_t target_size, float refresh_seconds);
 };
